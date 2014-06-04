d63243c9f0f1a4dfefbbf158586b76e96ef7bf0e
Daniel Williams <mail@daniel-williams.co.uk>
2014-06-04 17:52:29 +0100

    More docs changes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('../../src/python'))
-#sys.path.insert(0, os.path.abspath('../../src/python/pyflare'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../bayesflare'))
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
