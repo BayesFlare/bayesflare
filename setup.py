2dd52527c48ec68ea61b7401e66ed48901d5ed48
Daniel Williams <daniel.williams@ligo.org>
2015-11-18 12:24:41 +0000

    Updated the isntallation scripts to correct the problem with missing pyx files.

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ from Cython.Build import cythonize
 
 import os, sys
 
-ext_modules = [ Extension("stats.general", ["bayesflare/stats/general.pyx", "bayesflare/stats/log_marg_amp_full.c"], 
-                include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],  
-                library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]], 
-                libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+ext_modules = [ Extension("bayesflare.stats.general", 
+                          sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.c",], 
+                          include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],  
+                          library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]], 
+                          libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
 
-directives = {'embedsignature': True} # embed cython function signature in docstring
+directives = {'embedsignatjobsure': True} # embed cython function signature in docstring
 
 packs = ['bayesflare.data.data', 
          'bayesflare.models.model',
@@ -25,7 +26,8 @@ packs = ['bayesflare.data.data',
          'bayesflare.stats.general']
 setup(
   name = 'BayesFlare',
-  version = '1.0.0',
+  version = '1.0.2',
+url = 'https://github.com/BayesFlare/bayesflare',
   description = 'Python functions and classes implementing a Bayesian approach to flare finding.',
   author = 'Matthew Pitkin, Daniel Williams',
   author_email = 'matthew.pitkin@gla.ac.uk',
