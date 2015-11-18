4790d6f4e3c3bc15125b1ddea26e94ad89d2ffbf
Daniel Williams <daniel.williams@ligo.org>
2015-11-18 10:50:20 +0000

    Updated the setup script.

--- setup.py
+++ setup.py
@@ -9,10 +9,11 @@ from Cython.Build import cythonize
 
 import os, sys
 
-ext_modules = [ Extension("stats.general", ["bayesflare/stats/general.pyx", "bayesflare/stats/log_marg_amp_full.c"], 
-                include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],  
-                library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]], 
-                libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+ext_modules = [ Extension("bayesflare.stats.general", 
+                          sources=["bayesflare/stats/general.pyx", "bayesflare/stats/log_marg_amp_full.c"], 
+                          include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],  
+                          library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]], 
+                          libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
 
 directives = {'embedsignature': True} # embed cython function signature in docstring
 
@@ -27,7 +28,7 @@ packs = ['bayesflare.data.data',
          'bayesflare.stats.general']
 setup(
   name = 'BayesFlare',
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'Python functions and classes implementing a Bayesian approach to flare finding.',
   author = 'Matthew Pitkin, Daniel Williams',
   author_email = 'matthew.pitkin@gla.ac.uk',
