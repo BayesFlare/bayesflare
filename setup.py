622646dab5e578dc6e9a7b3bd3af863623621175
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-04-25 15:57:23 +0100

    setup.py: fixes to setup so that module build correctly again - refs #7

--- setup.py
+++ setup.py
@@ -1,41 +1,31 @@
 #! /usr/bin/env python
 
+from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
-import os, sys
+import os, sys, numpy
 
-ext_modules = [ Extension("bayesflare.stats.general", 
-                          sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.c",], 
-                          include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],  
-                          library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]], 
+ext_modules = [ Extension("bayesflare.stats.general",
+                          sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.pyx"],
+                          include_dirs=[numpy.get_include(), '.', os.popen('gsl-config --cflags').read()[2:-1]],
+                          library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
                           libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
 
 directives = {'embedsignatjobsure': True} # embed cython function signature in docstring
 
-packs = ['bayesflare.data.data', 
-         'bayesflare.models.model',
-         'bayesflare.noise.noise',
-         'bayesflare.misc.misc',
-         'bayesflare.simulate.simulate',
-         'bayesflare.inject.inject', 
-         'bayesflare.finder.find',
-         'bayesflare.stats.bayes',
-         'bayesflare.stats.general']
 setup(
-  name = 'BayesFlare',
+  name = 'bayesflare',
   version = '1.0.2',
 url = 'https://github.com/BayesFlare/bayesflare',
   description = 'Python functions and classes implementing a Bayesian approach to flare finding.',
   author = 'Matthew Pitkin, Daniel Williams',
   author_email = 'matthew.pitkin@gla.ac.uk',
-  packages = ['bayesflare'],
-  py_modules = packs,
+  packages = find_packages(),
   cmdclass = {'build_ext': build_ext},
-  ext_package='bayesflare',
-  ext_modules = cythonize(ext_modules, gdb_debug=False, compiler_directive=directives),
+  ext_modules = cythonize(ext_modules, gdb_debug=False, compiler_directives=directives),
   classifiers=[
       'Development Status :: 5 - Production/Stable',
       'Intended Audience :: Science/Research',
@@ -46,6 +36,6 @@ url = 'https://github.com/BayesFlare/bayesflare',
       'Natural Language :: English',
       'Topic :: Scientific/Engineering :: Astronomy',
       'Topic :: Scientific/Engineering :: Information Analysis'
-      
       ]
 )
+
