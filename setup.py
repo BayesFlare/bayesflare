a9c0cb356736361e051ac92fc325dcf0c4cdf023
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-07-07 15:25:48 +0100

    setup.py: further changes that hopefully allow things to work regardless of Cython being installed

--- setup.py
+++ setup.py
@@ -1,20 +1,48 @@
 #! /usr/bin/env python
 
-from setuptools import find_packages
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
-import os, sys, numpy
+#from setuptools import find_packages
+#from distutils.core import setup
+#from distutils.extension import Extension
+#from Cython.Distutils import build_ext
+#from Cython.Build import cythonize
 
-ext_modules = [ Extension("bayesflare.stats.general",
-                          sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.pyx"],
-                          include_dirs=[numpy.get_include(), '.', os.popen('gsl-config --cflags').read()[2:-1]],
-                          library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
-                          libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+import os, sys
 
-directives = {'embedsignatjobsure': True} # embed cython function signature in docstring
+# see https://stackoverflow.com/a/21621689/1862861 for why this is here
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+# check if cython is available in the system
+try:
+    import Cython
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+if use_cython:
+  ext_modules = [ Extension("bayesflare.stats.general",
+                            sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.pyx"],
+                            include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],
+                            library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
+                            libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+else:
+  ext_modules = [ Extension("bayesflare.stats.general",
+                            sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.c"],
+                            include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],
+                            library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
+                            libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+
+#directives = {'embedsignatjobsure': True} # embed cython function signature in docstring
 
 setup(
   name = 'bayesflare',
@@ -24,8 +52,12 @@ url = 'https://github.com/BayesFlare/bayesflare',
   author = 'Matthew Pitkin, Daniel Williams',
   author_email = 'matthew.pitkin@gla.ac.uk',
   packages = find_packages(),
+  package_data = {'': ['*.c', '*.h', '*.pyx']},
+  setup_requires = ['numpy'],
+  install_requires = ['numpy', 'scipy'],
   cmdclass = {'build_ext': build_ext},
-  ext_modules = cythonize(ext_modules, gdb_debug=False, compiler_directives=directives),
+  ext_modules = ext_modules,
+  #ext_modules = cythonize(ext_modules, gdb_debug=False, compiler_directives=directives),
   classifiers=[
       'Development Status :: 5 - Production/Stable',
       'Intended Audience :: Science/Research',
