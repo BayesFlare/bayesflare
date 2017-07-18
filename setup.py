d82b8b3994568ef3e54f2257e9765e5d6214e84e
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-07-18 10:18:58 +0100

    setup.py: removed somes comments and other minor changes

--- setup.py
+++ setup.py
@@ -6,14 +6,7 @@ from setuptools.command.build_ext import build_ext as _build_ext
 from setuptools.command.sdist import sdist  as _sdist
 import os, sys
 
-#from setuptools import find_packages
-#from distutils.core import setup
-#from distutils.extension import Extension
-#from Cython.Distutils import build_ext
-#from Cython.Build import cythonize
-
 cmdclass = { 'build_ext': _build_ext }
-
 
 # Make sure that when a source distribution gets rolled that the Cython files get rebuilt into C
 class sdist(_sdist):
@@ -42,19 +35,20 @@ else:
     use_cython = True
 
 if use_cython:
-  ext_modules = [ Extension("bayesflare.stats.general",
-                            sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.pyx"],
-                            include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],
-                            library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
-                            libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+    from Cython.Build import cythonize
+    directives = {'embedsignatjobsure': True} # embed cython function signature in docstring
+    ext_modules = [ Extension("bayesflare.stats.general",
+                              sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.pyx"],
+                              include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],
+                              library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
+                              libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+    ext_modules = cythonize(ext_modules, gdb_debug=False, compiler_directives=directives)
 else:
-  ext_modules = [ Extension("bayesflare.stats.general",
-                            sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.c"],
-                            include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],
-                            library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
-                            libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
-
-#directives = {'embedsignatjobsure': True} # embed cython function signature in docstring
+    ext_modules = [ Extension("bayesflare.stats.general",
+                              sources =[ "bayesflare/stats/log_marg_amp_full.c", "bayesflare/stats/general.c"],
+                              include_dirs=['.', os.popen('gsl-config --cflags').read()[2:-1]],
+                              library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]],
+                              libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
 
 setup(
     name = 'bayesflare',
@@ -62,12 +56,12 @@ setup(
     url = 'https://github.com/BayesFlare/bayesflare',
     description = 'Python functions and classes implementing a Bayesian approach to flare finding.',
     author = 'Matthew Pitkin, Daniel Williams',
-    author_email = 'matthew.pitkin@gla.ac.uk',
+    author_email = 'matthew.pitkin@glasgow.ac.uk',
     setup_requires=['numpy'],
     install_requires = ['numpy', 'scipy', 'cython', 'matplotlib', 'pyfits'],
     packages = find_packages(),
     cmdclass = cmdclass,
-    ext_modules = ext_modules, #cythonize(ext_modules, gdb_debug=False, compiler_directives=directives),
+    ext_modules = ext_modules,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
